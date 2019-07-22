ef62a8e83e306088e8c2887bea24d568bf1c9220
adrianhryn <36327238+adrianhryn@users.noreply.github.com>
2019-07-22 15:39:19 +0300

    Update server1.py

--- week6/server1.py
+++ week6/server1.py
@@ -3,16 +3,20 @@ import asyncio
 
 class ClientServerProtocol(asyncio.Protocol):
 
-    @staticmethod
-    def find_and_return_value(metric):
-
+    def read_database(self):
         data = []
         with open("log.txt", "r") as f:
             for i in f.readlines():
                 message = i.split(" ")
                 message.pop()
                 data.append([message[1], float(message[2]), int(message[3])])
+        return data
 
+    def find_and_return_value(self, metric):
+
+        data = self.read_database()
+
+        # returns all data from database
         if metric == "*":
             res = ""
             for i in data:
@@ -20,6 +24,8 @@ class ClientServerProtocol(asyncio.Protocol):
 
             return res
 
+        # if metric is not in the database, the func will return "" (due to task conditions)
+        # if metric is there, return its values and timestamps
         elif metric != "*":
             res = ""
             for i in data:
@@ -27,41 +33,43 @@ class ClientServerProtocol(asyncio.Protocol):
                     res += "{} {} {}".format(i[0], i[1], i[2]) + "\n"
             return res
 
-    @staticmethod
-    def formulate_response(command, metric, value, timestamp):
+    def formulate_response(self, command, metric, value, timestamp):
+        """
+        Chooses an appropriate server response according to what a client wrote
+        """
 
+        # server response for users get command
         if command == "get":
             print("ok")
-            print(ClientServerProtocol.find_and_return_value(metric))
+            print(self.find_and_return_value(metric))
 
+        # server response for users put command: write the value to the database
         if command == "put":
             print("ok\n")
             with open("log.txt", "a") as f:
                 f.write("{} {} {} {}".format(command, metric, value, timestamp) + " \n")
 
+        # if server received wrong typed command
         if command != "put" and command != "get":
             print("WRONG COMMAND")
 
     def data_received(self, data):
+        """
+        Receives message from some client, calculate which type is this
+        :param data: str
+        """
 
         print(data.decode("utf-8"))
-        if data.decode()[-1] == "*":
-            command, metric = data.decode("utf-8").split(" ")
-            value = -999
-            timestamp = -999
+        data = data.decode("utf-8").split(" ")
 
-        elif data.decode()[-1] != "*":
+        # if client types "get" command, we don't need the last two arguments in data
+        if len(data) == 2:
+            data.append("-999")
+            data.append("-999")
 
-            data = data.decode("utf-8").split(" ")
-            if len(data) == 2:
-                command, metric = data
-                value = -999
-                timestamp = -999
-            if len(data) == 4:
-                command, metric, value, timestamp = data
-
-        ClientServerProtocol.formulate_response(command, metric, value, timestamp)
-
+        command, metric, value, timestamp = data
+        to_server = ClientServerProtocol()
+        to_server.formulate_response(command, metric, value, timestamp)
 
 
 if __name__ == "__main__":
@@ -82,10 +90,3 @@ if __name__ == "__main__":
     server.close()
     loop.run_until_complete(server.wait_closed())
     loop.close()
-
-    #print(run_server("127.0.0.1", 8181))
-
-
-    def run_server(host, port):
-
-        pass
