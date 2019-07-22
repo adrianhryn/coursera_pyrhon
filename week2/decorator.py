02d753deff032d56447d87495ebe2c0aa3971d35
adrianhryn <36327238+adrianhryn@users.noreply.github.com>
2019-07-22 13:31:19 +0300

    Update decorator.py

--- week2/decorator.py
+++ week2/decorator.py
@@ -3,6 +3,7 @@ import json
 
 
 def to_json(func):
+    """Converts simple dictionary into json format"""
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
         result = func(*args, **kwargs)
@@ -13,13 +14,18 @@ def to_json(func):
 
 
 @to_json
-def get_data():
-    return {
+def get_data(dict_):
+    return dict_
+
+
+if __name__ == "__main__":
+
+    dict_ = {
         'data': 42,
         'data2': 21,
         'data3': {'data4': [1, 2, 3, 4, 5]}
     }
 
-
-print(get_data())  # вернёт '{"data": 42}'
-print(get_data.__name__)
+    assert get_data(dict_) == '{"data": 42, "data2": 21, "data3": {"data4": [1, 2, 3, 4, 5]}}'
+    assert get_data({'data': 42}) == '{"data": 42}'
+    assert get_data.__name__ == "get_data"
